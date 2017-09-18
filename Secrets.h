908623d10b08005ad31d239dc11515a1da1b426e
12voltman <schuhplattler42@gmail.com>
2017-09-18 14:54:58 -0400

    Initial commit

---
+++ Secrets.h
@@ -0,0 +1,8 @@
+
+#define ssid "____your-wifi-network-name_____"
+
+#define password "____your-wifi-network-password_____"
+#define mqtt_server "____MQTT-IP-address"
+
+#define compTopic "__topic-1__"
+#define projTopic "__topic-2__"
